3a043bedef65be2a55c949247cda07f26f976caf
Artur Andrzejczak <andrzejczak@dmcs.pl>
2015-10-22 22:07:25 +0000

    added upload config

--- upload_config.py
+++ upload_config.py
@@ -5,28 +5,15 @@ import time
 import struct
 import socket
 import sys
-import binascii
 
 def main():
-    file_size = 0
-    f = open("wvwms_config.bin", "rb")
-    try:
-        byte = f.read(1)
-        file_content = byte
-        while byte != "":
-            byte = f.read(1)
-            if byte != "":
-                file_content += byte
-            file_size+=1
-        print "Config file size: "
-        print file_size
-        print(binascii.hexlify(file_content))
-        s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-        s.bind(('', MYPORT))
-        data = bytearray([0x7A, 0xA7, 0x1+file_size , 0x0D]) + file_content
-        s.sendto(data, (MYGROUP_6, MYPORT))
+    s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    s.bind(('', MYPORT))
+    fh = open('wvwms_config.bin', 'rb')
+    config = bytearray(fh.read());
+    data = bytearray([0x7A, 0xA7, 0x1+len(config), 0x0D])
+    data = data + config
+    s.sendto(data, (MYGROUP_6, MYPORT))
 
-    finally:
-        f.close()
 if __name__ == '__main__':
     main()

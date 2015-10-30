586c793a9c53cba7ec39c40a648283ca6a9e26b7
Artur Andrzejczak <andrzejczak@dmcs.pl>
2015-10-30 20:59:56 +0000

    new command, improved udp raw receiver

---
+++ write_conf_reg.py
@@ -0,0 +1,19 @@
+MYPORT = 3000
+MYGROUP_6 = 'ff02::1%eth0'
+
+import time
+import struct
+import socket
+import sys
+
+def main():
+    s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    s.bind(('', MYPORT))
+    # writes to config reg 0x2, reverse bit order needed!
+    # reg addr, regl len, 3 bytes in reverse order
+    # 0x0F - 80Hz, 0x3D - 20Hz, 0x77 - 10Hz, 0x19 - 48Hz, 0x4B - 16Hz 
+    data = bytearray([0x7A, 0xA7, 0x7, 0x11, 0x2, 0x3, 0x17, 0x01, 0x80])
+    s.sendto(data, (MYGROUP_6, MYPORT))
+
+if __name__ == '__main__':
+    main()

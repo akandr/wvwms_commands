9af3b5bbbfe13f2bc6d2500bac26add425a29500
Artur Andrzejczak <andrzejczak@dmcs.pl>
2016-07-10 22:23:20 +0000

    Added new commands with value upload

--- write_buffer_size.py
+++ write_buffer_size.py
@@ -9,9 +9,10 @@ import sys
 def main():
     s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
     s.bind(('', MYPORT))
-    peak_sample = 7767800
-    data = bytearray([0x7A, 0xA7, 0x6, 0x27])
-    data+= bytearray( struct.pack("I", peak_sample) )
+    # MAX 5800
+    buffer_size = 5800
+    data = bytearray([0x7A, 0xA7, 0x6, 0x28])
+    data+= bytearray( struct.pack("I", buffer_size) )
     s.sendto(data, (MYGROUP_6, MYPORT))
 
 if __name__ == '__main__':
--- write_samplepack_size.py
+++ write_samplepack_size.py
@@ -9,9 +9,9 @@ import sys
 def main():
     s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
     s.bind(('', MYPORT))
-    peak_sample = 7767800
-    data = bytearray([0x7A, 0xA7, 0x6, 0x27])
-    data+= bytearray( struct.pack("I", peak_sample) )
+    samplepack_size = 3
+    data = bytearray([0x7A, 0xA7, 0x6, 0x30])
+    data+= bytearray( struct.pack("I", samplepack_size) )
     s.sendto(data, (MYGROUP_6, MYPORT))
 
 if __name__ == '__main__':

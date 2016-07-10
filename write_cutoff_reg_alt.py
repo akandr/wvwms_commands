a925f094abbe9ed9017506519766e60dfa9dafe6
Artur Andrzejczak <andrzejczak@dmcs.pl>
2016-07-10 21:56:59 +0000

    In writing values, automatic conversion of ints to bytes

---
+++ write_cutoff_reg_alt.py
@@ -0,0 +1,21 @@
+MYPORT = 3000
+MYGROUP_6 = 'ff02::1%eth0'
+
+import time
+import struct
+import socket
+import sys
+
+def main():
+    s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    s.bind(('', MYPORT))
+    cutoff_val = 7767800
+
+    # cutoff, 4 bytes in reverse order
+    # data = bytearray([0x7A, 0xA7, 0x6, 0x23, 0xF8, 0x86, 0x76, 0x00])
+    data = bytearray([0x7A, 0xA7, 0x6, 0x23])
+    data+= bytearray( struct.pack("I", cutoff_val) )
+    s.sendto(data, (MYGROUP_6, MYPORT))
+
+if __name__ == '__main__':
+    main()
